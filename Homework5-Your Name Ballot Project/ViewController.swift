36a88a3607683bed93176f2bda94832d87a1ed88
Allison Lowery <a.lowery@setonhill.edu>
2023-04-12 17:12:15 -0400

    Initial Commit

---
+++ Homework5-Your Name Ballot Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework5-Your Name Ballot Project
+//
+//  Created by Allie Lowery on 4/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
